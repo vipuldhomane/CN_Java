a3d0b9952d4e7d0b8c259602af5d9b77cdcf28bb
VipulD <vipuld04@gmail.com>
2023-04-27 13:17:07 +0530

    push

--- Module2/LInkedLists2/LLRecursion.java
+++ Module2/LInkedLists2/LLRecursion.java
@@ -2,6 +2,8 @@ package Module2.LInkedLists2;
 
 import java.util.Scanner;
 
+import javax.print.DocFlavor.INPUT_STREAM;
+
 class DoubleNode {
     Node<Integer> head;
     Node<Integer> tail;
@@ -150,11 +152,52 @@ public class LLRecursion {
 
     }
 
+    public static Node<Integer> mergerLL(Node<Integer> head1, Node<Integer> head2) {
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+        Node<Integer> t1 = head1, t2 = head2;
+        Node<Integer> head = null, tail = null;
+        if (t1.data <= t2.data) {
+            head = t1;
+            tail = t1;
+            t1 = t1.next;
+        } else {
+            head = t2;
+            tail = t2;
+            t2 = t2.next;
+        }
+        while (t1 != null && t2 != null) {
+            if (t1.data <= t2.data) {
+                tail.next = t1;
+                tail = t1;
+                t1 = t1.next;
+            } else {
+                tail.next = t2;
+                tail = t2;
+                t2 = t2.next;
+            }
+        }
+        // one list is over
+
+        if (t1 != null) {
+            tail.next = t1;
+        } else {
+            tail.next = t2;
+        }
+        return head;
+
+    }
+
     public static void main(String[] args) {
-        Node<Integer> head = takeInputN();
-        // head = reverseBest(head);
-        // printR(head);
-        System.out.println(Midpoint(head));
+        Node<Integer> head1 = takeInputN();
+        Node<Integer> head2 = takeInputN();
+        Node<Integer> head = mergerLL(head1, head2);
+        printR(head);
+        // System.out.println(Midpoint(head));
 
     }
 
